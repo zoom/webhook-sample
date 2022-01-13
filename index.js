e50af1c5a4c38f26864e587f1679c25acedcb16a
tommygaessler <tommy.gaessler@zoom.us>
2022-01-13 12:24:24 -0700

    updated response messages

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
 
   console.log(req.body)
-  console.log(req.headers);
+  console.log(req.headers)
 
   if (req.headers.authorization === process.env.ZOOM_WEBHOOK_VERIFICATION_TOKEN) {
 
+    var response = { message: 'Authorized request to Webhook Sample Node.js.', status: 200 }
+
+    console.log(response.message)
+
     // business logic here, example make API request to Zoom or 3rd party
 
-    res.status(200)
-    res.send()
+    res.status(response.status)
+    res.json(response)
   } else {
-    res.status(401)
-    res.send('Unauthorized request to Webhook Sample Node.js.')
+
+    var response = { message: 'Unauthorized request to Webhook Sample Node.js.', status: 200 }
+
+    console.log(response.message)
+
+    res.status(response.status)
+    res.json(response)
   }
 })
 
